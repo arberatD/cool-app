1b0505cdafaaa8c28bcd1112482745666c59f8a4
Muhammed Arslan <142781091+Muaz-Ar@users.noreply.github.com>
2024-02-06 12:30:29 +0100

    feat: post reques

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/mein-endpunkt', methods=['POST'])
+def mein_endpunkt():
+    data = request.get_json()
+    # Verarbeiten Sie hier Ihre Daten
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
