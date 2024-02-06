6ae3c34c5514ab35b262c8e51124da8221291149
Dennis <dennisdiepolder@outlook.com>
2024-02-06 12:26:51 +0100

    force route

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Welcome to my Flask API!"
 
+@app.route('/push-force-hard')
+def force():
+    return "Force push hard"
+
 if __name__ == '__main__':
     app.run(debug=True)
